e2c8f988052024af14f349c048867fa9657c910f
K4zoku <kazoku0356@gmail.com>
2022-03-12 13:30:10 +0700

    Added human-readable serialization

---
+++ src/main/java/io/github/k4zoku/kzgraph/model/graph/GraphSerializationMode.java
@@ -0,0 +1,7 @@
+package io.github.k4zoku.kzgraph.model.graph;
+
+public enum GraphSerializationMode {
+    ADJACENCY_LIST,
+    ADJACENCY_MATRIX,
+    LABELED_ADJACENCY_MATRIX
+}
---
+++ src/main/java/io/github/k4zoku/kzgraph/model/serialization/HumanSerializable.java
@@ -0,0 +1,14 @@
+package io.github.k4zoku.kzgraph.model.serialization;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.text.ParseException;
+
+public interface HumanSerializable {
+
+    void serialize(Writer writer) throws IOException;
+
+    void deserialize(Reader reader) throws ParseException, IOException;
+
+}
